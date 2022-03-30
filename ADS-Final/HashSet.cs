53f3911693a541d548ea3be1fe8cee330daea1b7
Elliott <ellduc4@gmail.com>
2022-03-29 20:38:02 -0500

    start laying out the class

--- ADS-Final/HashSet.cs
+++ ADS-Final/HashSet.cs
@@ -2,5 +2,25 @@
 
 public class HashSet
 {
-    
+    private uint capacity;
+    private uint[] array;
+
+    public HashSet(uint capacity)
+    {
+        this.capacity = capacity;
+        array = new uint[capacity];
+    }
+
+    public void Add(string value)
+    {
+    }
+
+    public void Remove(string value)
+    {
+    }
+
+    public uint Search(string value)
+    {
+        return 1;
+    }
 }
